b19d9a1b17479f40985c6557a64dac8122cc0373
gamp <bramtechxxvi@gmail.com>
2026-09-23 11:48:13 +0100

    env call implemetation

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,23 @@
-od
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+
+
+class Settings(BaseSettings):
+    app_name: str = "URL Shortener API"
+    app_env: str= "development"
+    database_url: str
+    base_url: str= "http://localhost:8000"
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+    
+    
+@lru_cache
+def get_settings()-> Settings:
+    return Settings()
