2cfc8e5dd16db4161a0596c87f1a718363d6e525
gamp <bramtechxxvi@gmail.com>
2026-09-23 14:41:09 +0100

    created a test file for testing short code in tests/unit/

---
+++ backend/tests/unit/test_short_code.py
@@ -0,0 +1,8 @@
+from app.utils.short_code import generate_short_code
+
+
+def test_generate_short_code_returns_six_characters():
+    short_code = generate_short_code()
+    assert len(short_code) == 6
+    
+def test_generate_short_code
