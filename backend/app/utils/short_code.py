0d05c40cc4b8c38c732dfc3f834fed6c8dd0f517
gamp <bramtechxxvi@gmail.com>
2026-09-23 14:49:52 +0100

    implemented generate_short_code in util/short_code and test generate short code returns 6 characters

--- backend/app/utils/short_code.py
+++ backend/app/utils/short_code.py
@@ -4,3 +4,11 @@ import secrets
 
 CHARACTERS = string.ascii_letters + string.digits
 
+
+def generate_short_code(lenght: int=6)-> str:
+    return "".join(
+        
+        secrets.choice(CHARACTERS)
+        for _ in range(6)
+    )
+    
