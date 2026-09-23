600957e76ee04d136b00325456ef2247794f685d
gamp <bramtechxxvi@gmail.com>
2026-09-23 14:43:10 +0100

    created a test file for testing short code in tests/unit/

--- backend/tests/unit/test_short_code.py
+++ backend/tests/unit/test_short_code.py
@@ -5,4 +5,7 @@ def test_generate_short_code_returns_six_characters():
     short_code = generate_short_code()
     assert len(short_code) == 6
     
-def test_generate_short_code
+    
+def test_generate_short_code_contains_only_base62_characters():
+    short_code = generate_short_code()
+    assert short_code.isalnum()
