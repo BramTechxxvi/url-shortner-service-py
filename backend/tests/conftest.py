9d21dd9784c88707176e054c7e506d7f7e0fdbd9
gamp <bramtechxxvi@gmail.com>
2026-09-24 11:25:54 +0100

    added test db to config so as to begin integration test

---
+++ backend/tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import get_settings
+from app.database import Base
+import app.models
+
+
+
+settings = get_settings()
+
+
+@pytest.fixture(scope="session")
+def test_engine():
+    engine = create_engine(settings.test_database_url, pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    yield engine
+    
+    Base.metadata.drop_all(bind=engine)
+    engine.dispose()
